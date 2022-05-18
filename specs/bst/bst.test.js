32e736918b784c978817e8ff73c5fc363c1e3bdb
Akinwunmi Samuel <88426040+Kouzin1@users.noreply.github.com>
2022-05-18 10:49:31 +0100

    Binary Search Tree

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -18,6 +18,44 @@ right - Node/object - the right node which itself may be another tree
 
 class Tree {
   // code goes here
+  constructor(){
+    this.root = null;
+  }
+  add(value){
+    if (this.root === null){
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true){
+        if (current.value > value) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
+  }
+  toObject(){
+    return this.root;
+  }
+}
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 // you might consider using a Node class too
