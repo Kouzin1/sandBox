89cc2db03d4a76312cc243cc0180ac63833aac9d
Akinwunmi Samuel <88426040+Kouzin1@users.noreply.github.com>
2022-05-18 17:38:43 +0100

    Self Balancing avl tree

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,11 +14,11 @@
 */
 
 function insertionSort(nums) {
-  for (let i = 1; i < nums.length; i==) {
+  for (let i = 1; i < nums.length; i++) {
     let numberToInsert = nums[i];
     let j;
 
-    for (j = i -1; nums[j] > numberToinsert && j >= 0, j--) {
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
       nums[j + 1] = nums[j];
     }
     nums[j + 1] = numberToInsert;
